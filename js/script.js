2610e81c1fd1b1fe25b58d4c73d0621f18fd3f94
JaimeMartinezCuevas <jaimemarcudesigner@gmail.com>
2023-11-08 20:11:09 +0100

    second commit (js fixed)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-// Necesito una variable que capture la xugá'l usuariu y otra de la máquina, asina que entós debería de entamalo per ahí
-// Depués creo que pue facese col bucle if else en plan; xugaor = tixoria && máquina = papel { ganaste } mas menos
-// Bueno y faeme falta pone-y lo de los puntos
-
 const botonesJugada = document.querySelectorAll(".boton-jugada");
 const resultados = document.getElementById("resultados");
 const contadorUsuario = document.getElementById("contador-usuario");
@@ -11,41 +7,47 @@ let puntosUsuario = 0;
 let puntosOrdenador = 0;
 
 botonesJugada.forEach(boton => {
-
   boton.addEventListener("click", () => {
+    const jugadaUsuario = boton.getAttribute("data-jugada");
 
     const jugadas = ["piedra", "papel", "tijera"];
-    const jugadaUsuario = boton.getAttribute("data-jugada");
     const jugadaOrdenador = jugadas[Math.floor(Math.random() * 3)];
+
+    // const jugadaOrdenador = jugadas[1] (Así siempre sale papel);
+
+    //He tenido que buscar cómo hacer que la máquina escogiera entre las tres opciones.
+    //Pensando, he considerado crear un array con las tres opciones para que según el número (0, 1 o 2), la máquina eligiera
+    //De todas formas seguía sin saber cómo hacer que javascript generase un número aleatorio entre el 0 y el 2 por lo que lo he buscado
+    //y he dado con la opción de usar math random para indicar que el valor sea aleatorio. El multiplicarlo por tres indica que las opciones son tres, ya que se abarcan el 0, 1 y 2, siendo tres números (piedra, papel y tijera en el array)
+    //Por último, al ver que seguía sin funcionar, he vuelto a investigar y he visto que math floor era una función que no conocía pero por lo visto es necesaria para que los números que se generasen al azar fueran enteros, sin decimales, siendo esta la razón de que no funcionase.
+
 
     const resultado = compararJugadas(jugadaUsuario, jugadaOrdenador);
 
-    if (resultado === "Has ganado") {
-      puntosUsuario++;
-    } else if (resultado === "Has perdido") {
-      puntosOrdenador++;
-    }
-
     contadorUsuario.textContent = `Tus puntos: ${puntosUsuario}`;
     contadorOrdenador.textContent = `Puntos de la máquina: ${puntosOrdenador}`;
-
   });
 });
 
 function compararJugadas(jugadaUsuario, jugadaOrdenador) {
+  
+    let resultado;
+
   if (jugadaUsuario === jugadaOrdenador) {
-
-    resultados.textContent = `Elegiste ${jugadaUsuario}, y la máquina ha elegido ${jugadaOrdenador}, por lo que ha habido un empate.`;
-    
+    resultado = `Elegiste ${jugadaUsuario}, y la máquina ha elegido ${jugadaOrdenador}, por lo que ha habido un empate.`;
   } else if (
-    (jugadaUsuario === "piedra" && jugadaOrdenador === "tijera") || (jugadaUsuario === "papel" && jugadaOrdenador === "piedra") || (jugadaUsuario === "tijera" && jugadaOrdenador === "papel")
+    (jugadaUsuario === "piedra" && jugadaOrdenador === "tijera") ||
+    (jugadaUsuario === "papel" && jugadaOrdenador === "piedra") ||
+    (jugadaUsuario === "tijera" && jugadaOrdenador === "papel")
   ) {
-
-    resultados.textContent = `Elegiste ${jugadaUsuario}, y la máquina ha elegido ${jugadaOrdenador}, por lo que has ganado.`;
-    
+    resultado = `Elegiste ${jugadaUsuario}, y la máquina ha elegido ${jugadaOrdenador}, por lo que has ganado.`;
+    puntosUsuario++;
   } else {
-
-    resultados.textContent = `Elegiste ${jugadaUsuario}, y la máquina ha elegido ${jugadaOrdenador}, por lo que has perdido.`;
-
+    resultado = `Elegiste ${jugadaUsuario}, y la máquina ha elegido ${jugadaOrdenador}, por lo que has perdido.`;
+    puntosOrdenador++;
   }
+
+  resultados.textContent = resultado;
+
+  return resultado;
 }
